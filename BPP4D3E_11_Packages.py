cf6eeac54296c99f1d035b487e30a7c5463d452a
John Paul Mueller <John@JohnMuellerBooks.com>
2022-05-05 16:38:42 -0500

    Chapter 11 Source Code Files
    Discover how to use packages in Python, including both the conda and pip utilties.

---
+++ BPP4D3E_11_Packages.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Creating Code Groupings
+
+# ## Creating your first package
+
+# In[1]:
+
+
+def SayHello(Name):
+   print("Hello ", Name)
+   return
+def SayGoodbye(Name):
+   print("Goodbye ", Name)
+   return
+
+
+# In[ ]:
+
+
+
+
